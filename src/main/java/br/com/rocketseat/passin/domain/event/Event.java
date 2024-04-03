a3adbf12a1e598ee638ecfce845c5c90c80be828
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-04-02 22:34:17 -0300

    feat: add event entity

---
+++ src/main/java/br/com/rocketseat/passin/domain/event/Event.java
@@ -0,0 +1,30 @@
+package br.com.rocketseat.passin.domain.event;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "events")
+public class Event {
+    @Id
+    @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String details;
+
+    @Column(nullable = false, unique = true)
+    private String slug;
+
+    @Column(name = "maximum_attendees", nullable = false)
+    private Integer maximumAttendees;
+}
