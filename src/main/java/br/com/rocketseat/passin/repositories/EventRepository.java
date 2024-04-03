d3c0abbb9a5fba04c769942e4b0f28ce581f6257
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-04-02 22:35:45 -0300

    feat: add event repository

---
+++ src/main/java/br/com/rocketseat/passin/repositories/EventRepository.java
@@ -0,0 +1,7 @@
+package br.com.rocketseat.passin.repositories;
+
+import br.com.rocketseat.passin.domain.event.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventRepository extends JpaRepository<Event, String> {
+}
