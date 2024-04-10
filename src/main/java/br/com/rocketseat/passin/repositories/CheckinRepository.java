11712a718f89910cc80fbd9c6f7c51e4c30a6e61
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-04-10 14:08:45 -0300

    feat: add findByAttendeeId function to checkin repository

--- src/main/java/br/com/rocketseat/passin/repositories/CheckinRepository.java
+++ src/main/java/br/com/rocketseat/passin/repositories/CheckinRepository.java
@@ -3,5 +3,9 @@ package br.com.rocketseat.passin.repositories;
 import br.com.rocketseat.passin.domain.checkin.CheckIn;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
+
 public interface CheckinRepository extends JpaRepository<CheckIn, Integer> {
+    Optional<CheckIn> findByAttendeeId(String attendeeId);
 }
