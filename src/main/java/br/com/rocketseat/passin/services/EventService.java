650db16a804df838ad95adf489453a707c6b659e
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-04-10 14:16:29 -0300

    feat: add event service

---
+++ src/main/java/br/com/rocketseat/passin/services/EventService.java
@@ -0,0 +1,51 @@
+package br.com.rocketseat.passin.services;
+
+import br.com.rocketseat.passin.domain.attendee.Attendee;
+import br.com.rocketseat.passin.domain.event.Event;
+import br.com.rocketseat.passin.domain.event.exceptions.EventNotFoundException;
+import br.com.rocketseat.passin.dto.event.EventIdDTO;
+import br.com.rocketseat.passin.dto.event.EventRequestDTO;
+import br.com.rocketseat.passin.dto.event.EventResponseDTO;
+import br.com.rocketseat.passin.repositories.EventRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.text.Normalizer;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class EventService {
+    private final EventRepository eventRepository;
+    private final AttendeeService attendeeService;
+
+    public EventResponseDTO getEventDetail(String eventId) {
+        var event = this.eventRepository.findById(eventId)
+                .orElseThrow(() -> new EventNotFoundException("Event not found with ID: " + eventId));
+        List<Attendee> attendeeList = this.attendeeService.getAllAttendeesFromEvent(eventId);
+
+        return new EventResponseDTO(event, attendeeList.size());
+    }
+
+    public EventIdDTO createEvent(EventRequestDTO eventDTO) {
+        Event newEvent = new Event();
+
+        newEvent.setTitle(eventDTO.title());
+        newEvent.setDetails(eventDTO.details());
+        newEvent.setMaximumAttendees(eventDTO.maximumAttendees());
+        newEvent.setSlug(createSlug(eventDTO.title()));
+
+        this.eventRepository.save(newEvent);
+
+        return new EventIdDTO(newEvent.getId());
+    }
+
+    private String createSlug(String text) {
+        String normalize = Normalizer.normalize(text, Normalizer.Form.NFD);
+        return normalize
+                .replaceAll("[\\p{InCOMBINING_DIACRITICAL_MARKS}]", "")
+                .replaceAll("[^\\w\\s]", "")
+                .replaceAll("\\s+", "-")
+                .toLowerCase();
+    }
+}
