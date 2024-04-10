2aa916dd4c8c79ed42bdfe7fd89062b2656e3c6f
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-04-10 14:12:50 -0300

    feat: add event not found exception

---
+++ src/main/java/br/com/rocketseat/passin/domain/event/exceptions/EventNotFoundException.java
@@ -0,0 +1,7 @@
+package br.com.rocketseat.passin.domain.event.exceptions;
+
+public class EventNotFoundException extends RuntimeException {
+    public EventNotFoundException(String message) {
+        super(message);
+    }
+}
