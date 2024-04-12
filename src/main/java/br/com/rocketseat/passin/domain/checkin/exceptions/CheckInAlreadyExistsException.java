e37ece2e5f39e5fb7ebc9596fff282854b771443
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-04-11 21:28:24 -0300

    feat: create CheckInAlreadyExistsException

---
+++ src/main/java/br/com/rocketseat/passin/domain/checkin/exceptions/CheckInAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package br.com.rocketseat.passin.domain.checkin.exceptions;
+
+public class CheckInAlreadyExistsException extends RuntimeException {
+    public CheckInAlreadyExistsException(String message) {
+        super(message);
+    }
+}
