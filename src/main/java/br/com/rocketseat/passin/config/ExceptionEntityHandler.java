c6d05f76bfa10bb3309fde2197e4ed3260a27ba2
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-04-11 21:45:56 -0300

    feat: add handleCheckInAlreadyExists fucntion to ExceptionEntityHandler

--- src/main/java/br/com/rocketseat/passin/config/ExceptionEntityHandler.java
+++ src/main/java/br/com/rocketseat/passin/config/ExceptionEntityHandler.java
@@ -2,6 +2,7 @@ package br.com.rocketseat.passin.config;
 
 import br.com.rocketseat.passin.domain.attendee.exceptions.AttendeeAlreadyExistsException;
 import br.com.rocketseat.passin.domain.attendee.exceptions.AttendeeNotFoundException;
+import br.com.rocketseat.passin.domain.checkin.exceptions.CheckInAlreadyExistsException;
 import br.com.rocketseat.passin.domain.event.exceptions.EventNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +23,11 @@ public class ExceptionEntityHandler {
 
     @ExceptionHandler(AttendeeAlreadyExistsException.class)
     public ResponseEntity<Void> handleAttendeeAlreadyExists(AttendeeAlreadyExistsException exception) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).build();
+    }
+
+    @ExceptionHandler(CheckInAlreadyExistsException.class)
+    public ResponseEntity<Void> handleCheckInAlreadyExists(CheckInAlreadyExistsException exception) {
         return ResponseEntity.status(HttpStatus.CONFLICT).build();
     }
 }
